bd5afdaea270ce13fc206c022b12e4a6498a3ae4
jippe <jippehoogeveen1@gmail.com>
2025-04-03 13:47:00 +0200

    Eertree gefixed

--- strings/eertree.cpp
+++ strings/eertree.cpp
@@ -1,13 +1,13 @@
-#define MAXN 100100
-#define SIGMA 26
-#define BASE 'a'
+const int MAXN = 100100;
+const int SIGMA = 26;
+const char BASE = 'a';
 char *s = new char[MAXN];
 struct state {
   int len, link, to[SIGMA];
 } *st = new state[MAXN+2];
 struct eertree {
-  int last, sz, n;
-  eertree() : last(1), sz(2), n(0) {
+  int last, size, n;
+  eertree() : last(1), size(2), n(0) {
     st[0].len = st[0].link = -1;
     st[1].len = st[1].link = 0; }
   int extend() {
@@ -15,7 +15,7 @@ struct eertree {
     while (n - st[p].len - 2 < 0 || c != s[n - st[p].len - 2])
       p = st[p].link;
     if (!st[p].to[c-BASE]) {
-      int q = last = sz++;
+      int q = last = size++;
       st[p].to[c-BASE] = q;
       st[q].len = st[p].len + 2;
       do { p = st[p].link;
