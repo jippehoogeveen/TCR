b79fcd3fdcddaedbb35b7f4e0cc605c60f4aa654
jippe <jippehoogeveen1@gmail.com>
2025-04-08 18:24:09 +0200

    voorstel simpelere code order tree

--- code/datastructures/order_statistic.cc
+++ code/datastructures/order_statistic.cc
@@ -1,6 +1,6 @@
 #include <bits/extc++.h>
 using namespace __gnu_pbds;
-template<class TK,class TM> using order_tree = tree<TK,TM,greater<TK>,rb_tree_tag, tree_order_statistics_node_update>;
+template<class TK,class TM> using order_tree = tree<TK,TM,less<TK>,rb_tree_tag, tree_order_statistics_node_update>;
 template<class TK> using order_set = order_tree<TK,null_type>;
 
 vi s;
@@ -19,3 +19,11 @@ signed main() {
 	cout << t.find_by_order( 2 )->y << endl;
 	cout << t.order_of_key( ii{s[3],3} ) << endl;
 }
+/*
+orderset<ll> s;
+s.insert(5);
+s.insert(2);
+s.insert(3);
+s.find_by_order(2);//5
+s.order_of_key(3);//1
+s.order_of_key(4);//2*/
