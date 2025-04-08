2b4b5fac196330eef4dfaea3a6d38f38dd9f8b52
jippe <jippehoogeveen1@gmail.com>
2025-04-08 17:57:45 +0200

    primitieve wortel versimpeld

--- code/math/primitive_root.cpp
+++ code/math/primitive_root.cpp
@@ -1,12 +1,12 @@
 ll primitive_root(ll m) {
-	vector<ll> div; ll phi = m - 1;
-	for (ll i = 1; i*i < phi; i++)
+	vi div; ll phi = m - 1;
+	for (ll i = 2; i*i <= phi; i++)
 		if (phi % i == 0) {
-			if (i < phi) div.pb(i);
-			if (phi/i < phi) div.pb(phi/i);
+			div.pb(i);
+			div.pb(phi/i);
 		}
 	rep(x,2,m) {
-		bool ok = true;
+		bool ok = true;//gcd-check
 		for (ll d : div) if (mod_pow(x, d, m) == 1)
 			{ ok = false; break; }
 		if (ok) return x;
