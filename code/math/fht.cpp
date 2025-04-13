c1d2fd919d171852733bfeb0e6ec1661705d5cda
jippe <jippehoogeveen1@gmail.com>
2025-04-13 13:12:47 +0200

    Convolutiefunctie bij FHT geschreven

--- code/math/fht.cpp
+++ code/math/fht.cpp
@@ -1,4 +1,4 @@
-void fht(vi &A, bool inv=false, int l, int r) {
+void fht(vi &A, bool inv, int l, int r) {
 	if (l+1 == r) return;
 	int k = (r-l)/2;
 	if (!inv) fht(A, inv, l, l+k), fht(A, inv, l+k, r);
@@ -8,4 +8,12 @@ void fht(vi &A, bool inv=false, int l, int r) {
 		else      A[i] = (x+y)/2, A[i+k] = (-x+y)/2;
 	}
 	if (inv) fht(A, inv, l, l+k), fht(A, inv, l+k, r);
+}
+
+vi conv(vi A, vi B) {
+	int n = sz(A);
+	fht(A,false,0,n); fht(B,false,0,n);
+	vi res = vi(n); REP(i,n) res[i] = A[i] * B[i];
+	fht(res,true,0,n);
+	return res;
 }
