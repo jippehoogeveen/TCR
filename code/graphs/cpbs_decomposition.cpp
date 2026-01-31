2598782923cc6b33eba1980c6ab62aab7b632a75
jippe <jippehoogeveen1@gmail.com>
2026-01-31 11:26:25 +0100

    Verbeterde bicomp decompositie

--- code/graphs/cpbs_decomposition.cpp
+++ code/graphs/cpbs_decomposition.cpp
@@ -1,54 +1,66 @@
-struct cpdecomp {
-    ll n; vvi adj;
-    set<ii> bs; vb cut, split;
-    vb seen; vi depth;
-    vvi comps, adjcomp;
-    cpdecomp(vvi& _adj) : n(sz(_adj)) {
-        pair<vi, vii> cpbs = cut_points_and_bridges(_adj);
-        for(ii b : cpbs.y) bs.emplace(ii(min(b.x,b.y), max(b.x,b.y)));
-        cut = split = vb(n, false);
-        for(ll c : cpbs.x) cut[c] = true;
-        REP(i, n) if(sz(_adj[i]) == 1) cut[i] = true;
-        adj = vvi(n);
-        REP(i, n) for(ll j : _adj[i])
-            if(bs.count(ii(min((ll)i,j), max((ll)i,j))) == 0)
-                adj[i].pb(j);
-        decomp();
-    }
-    void decomp() {
-        adjcomp = vvi(n); seen = vb(n, false); depth = vi(n);
-        REP(i, n) if(!seen[i]){
-            vi c; makecomp(i, c, -1); decomp(c);
-        }
-        for(ii b : bs) addedge(b.x, b.y);
-    }
-    void decomp(vi& c) {
-        vi cp;
-        for(ll x : c) if(cut[x] && !split[x]) cp.pb(x);
-        if(sz(cp) == 0) {
-            comps.pb(c); adjcomp.pb(vi());
-            for(ll x : c) if(cut[x]) addedge(x, sz(comps) - 1 + n);
-            return;
-        }
-        for(ll x : c) {
-            seen[x] = false;
-            depth[x]++;
-        }
-        ll s = cp[rand() % sz(cp)];
-        split[s] = true;
-        for(ll i : adj[s]) if(!seen[i]) {
-            seen[s] = false; vi d;
-            makecomp(i, d, s); decomp(d);
-        }
-        for(ll x : c) depth[x]--;
-    }
-    void makecomp(ll i, vi& c, ll s) {
-        if(seen[i]) return;
-        seen[i] = true; c.pb(i);
-        if(i == s) return;
-        for(ll j : adj[i])
-            if(depth[j] == depth[i])
-                makecomp(j, c, s);
-    }
-    void addedge(ll u, ll v) { adjcomp[u].pb(v); adjcomp[v].pb(u); }
-};
+struct bicomp {
+	vvi adj; int n;
+	vi low, num, block, cp; vvi blocks;
+	vii bs; int curnum;
+
+	void dfs(int u, int p) {
+		block.pb(u); low[u] = num[u] = curnum++;
+		int cnt = 0; bool found = false;
+		for(int v : adj[u]) {
+			if (num[v] == -1) {
+				dfs(v, u);
+				low[u] = min(low[u], low[v]);
+				cnt++;
+				found = found || low[v] >= num[u];
+				if (low[v] > num[u]) bs.eb(u, v);
+				if(low[v] >= num[u]) {
+					vi b2; while(true) {
+						int cur = block.back(); block.pop_back();
+						b2.pb(cur); if(cur == v) break; }
+					if(low[v] == num[u]) {
+						b2.pb(u); blocks.pb(b2); }
+				}
+			} else if (p != v) low[u] = min(low[u], num[v]);
+		}
+		if (found && (p != -1 || cnt > 1)) cp.pb(u); }
+
+	bicomp(vvi &_adj) : n(sz(_adj)), adj(_adj) {
+		num = vi(n, -1); low = vi(n);
+		curnum = 0;
+		REP(i,n) if (num[i] < 0) dfs(i, -1); }
+
+	vvi blockadj; vb iscut; set<ii> bsset;
+	void addedge(ll u, ll v){ blockadj[u].pb(v); blockadj[v].pb(u); }
+	void makecpdecomp() {
+		for(ii p : bs) bsset.emplace(ii(min(p.x,p.y), max(p.x,p.y)));
+		iscut = vb(n, false);
+		for(ll x : cp) iscut[x] = true;
+		REP(i, n) iscut[i] = iscut[i] || (sz(adj[i]) <= 1);
+		blockadj = vvi(n + sz(blocks));
+		REP(i, sz(blocks)) for(ll x : blocks[i])
+			if(iscut[x]) addedge(i + n, x);
+		for(ii p : bs) addedge(p.x, p.y); }
+
+	vector<map<ll,vi>> inblockedge; vi wblock;
+	void edgeinblock(ll b, ll u, ll v) {
+		if(inblockedge[b].count(u) == 0) inblockedge[b].emplace(u, vi());
+		inblockedge[b][u].pb(v); }
+	void dfsbe(ll i, ll p) {
+		for(ll j : blockadj[i])
+			if(j != p && (i >= n || j >= n)) dfsbe(j, i);
+		if(i >= n) {
+			for(ll x : blocks[i - n]) wblock[x] = i - n;
+			for(ll j : blockadj[i])
+				if(j < n && j != p)
+					for(ll k : adj[j])
+						if(bsset.count(ii(min(j, k), max(j, k))) == 0)
+							edgeinblock(wblock[k], j, k);
+		} }
+	void makeblockedges() {
+		inblockedge = vector<map<ll,vi>>(sz(blocks));
+		wblock = vi(n, -1);
+		REP(i, sz(blocks)) {
+			if (wblock[blocks[i][0]] == -1) dfsbe(i + n, -1);
+			for(ll j : blocks[i]) if(!iscut[j])
+					for(ll k : adj[j]) edgeinblock(i, j, k); }
+	} };
